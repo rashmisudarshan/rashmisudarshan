00e67180112fb571ff1f12ebeccef88ae1e10b95
Rashmi_Sudarshan <rashmisudarshan01@gmail.com>
2024-06-11 14:57:21 -0400

    Adding built files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/rashmisudarshan.github.io",
+  base: "/rashmisudarshan.github.io/",
   plugins: [react()],
 })
